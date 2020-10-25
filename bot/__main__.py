3b0f54c3b57e516eb2907511f7d7c7b005778f0e
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-25 12:20:55 +0100

    Added a basic level logging info

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,16 @@
 import os
+import logging
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = commands.Bot(command_prefix='!')
 TOKEN = os.getenv('TOKEN')
