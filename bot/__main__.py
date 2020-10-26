7dd19e44315590748ae2d65661acee7e15b1d687
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-26 15:23:29 +0100

    Added intents as the new API requires

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,18 +12,30 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.reactions = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
 TOKEN = os.getenv('TOKEN')
 
 
 @client.event
 async def on_ready():
     print('Bot is ready')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='Transport Tycoon'))
 
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
+
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int):
+    def check(m):
+        return not m.pinned
+    await ctx.channel.purge(limit=amount+1)
 
 
 @client.command()
@@ -52,6 +64,10 @@ for filename in os.listdir('./bot/cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
         print(f'{filename[:-3]} loaded')
 
+
+@client.event
+async def on_command_error(ctx, error):
+    print(type(error), error)
 
 
 client.run(TOKEN)
