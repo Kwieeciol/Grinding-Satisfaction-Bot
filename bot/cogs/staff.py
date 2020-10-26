3e50b35256fe30b3347dc18b8b049237e74a61ff
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-26 19:18:08 +0100

    Changed parameters into the order functions

--- bot/cogs/staff.py
+++ bot/cogs/staff.py
@@ -16,45 +16,67 @@ class Staff(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    async def take_order(self, payload, channel):
-        message = await channel.fetch_message(payload.message_id)
-        user = discord.utils.get(channel.guild.members, id=payload.user_id)
-        if (embed := _is_embed(message)):
+    async def take_order(self, ctx):
+        if (embed := _is_embed(ctx.message)):
             id = return_int(embed.title)
             # Getting the roles
-            customer_role = discord.utils.get(channel.guild.roles, name=f'GS-{id}')
-            worker_role = discord.utils.get(channel.guild.roles, name=f'GS-{id} worker')
-            management_role = discord.utils.get(channel.guild.roles, id=MODERATION_ROLES)
+            customer_role = discord.utils.get(ctx.guild.roles, name=f'GS-{id}')
+            worker_role = discord.utils.get(ctx.guild.roles, name=f'GS-{id} worker')
+            management_role = discord.utils.get(ctx.guild.roles, id=MODERATION_ROLES)
             # The channel permissions overwrites
             overwrites = {
-                channel.guild.default_role: PermissionOverwrite(read_messages=False),
+                ctx.guild.default_role: PermissionOverwrite(read_messages=False),
                 customer_role: PermissionOverwrite(read_messages=True, send_messages=False),
                 worker_role: PermissionOverwrite(read_messages=True, send_messages=True),
                 management_role: PermissionOverwrite(read_messages=True, send_messages=True)
             }
 
-            category = discord.utils.get(channel.guild.categories, id=Categories.in_progress)
+            category = discord.utils.get(ctx.guild.categories, id=Categories.in_progress)
             # Channel creation
-            order_channel = await channel.guild.create_text_channel(f'GS-{id}', overwrites=overwrites, category=category)
-            # Updating the embed with a new field - 'Progress'
+            order_channel = await ctx.guild.create_text_channel(f'GS-{id}', overwrites=overwrites, category=category)
+            # Adding the roles
+            await ctx.author.add_roles(worker_role)
+            # Updating the embed with a new field - 'Progress', sending and pinning it
             embed.add_field(name='Progress', value='0', inline=True)
-            # Sending the embed and pinning it
-            channel_message = await order_channel.send(embed=embed)
-            await channel_message.pin()
+            message = await order_channel.send(embed=embed)
+            await message.pin()
             # Deleting the old message
-            await message.delete()
-            await user.add_roles(worker_role)
+            await ctx.message.delete()
             # Assigning the order to the worker in the database
-            await database.assign_order(id, user.id)
+            # await database.assign_order(id, ctx.author.id)
+
+
+    async def change_status(self, ctx):
+        # --to do--
+        # 1. Check if the reacted message is an embed
+        # 2. Check if the progress is the same as the ordered amount
+        # 3. Ask the worker if he sure he want to procedure
+        # 4. Move the channel to 'pending-collection' category
+        # 5. Change the permissions for the worker role to: send_messages = False
+        # 6. Remove the reactions in the embed
+        # 7. Delete all the messages apart from the pinned ones (embed)
+        pass
+
+
+    async def finish_order(self, ctx):
+        # --to do--
+        # 1. Check if the reacted message is an embed
+        # 2. Delete the channel and roles
+        # 3. Change the status of the order in the database to 'completed'
+        pass
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        channel = self.client.get_channel(payload.channel_id)
         if payload.user_id != self.client.user.id:
+            channel = self.client.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            author = channel.guild.get_member(payload.user_id)
+            ctx = await self.client.get_context(message)
+            ctx.author = author
             if channel.id == Channels.queued:
                 # The reaction has been added in the 'queued' channel
-                await self.take_order(payload, channel)
+                await self.take_order(ctx)
 
             elif channel.category != None:
                 if channel.category.id == Categories.in_progress:
