baec4d2c16b62b1f4873d10fc0d783dbbda89802
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-27 19:24:59 +0100

    Added 'edit' command for management

--- bot/cogs/management.py
+++ bot/cogs/management.py
@@ -1,10 +1,14 @@
+from typing import Optional
 import discord
 from discord.ext import commands
+import resources.database as database
+from resources.functions import edit_embed
 from resources.constants import MODERATION_ROLES, COLOUR, EMOJI_ID, Categories
 
 
 def convert_args(args):
 	available_options = ['amount', 'storage', 'discount', 'priority']
+	print(args)
 
 	if len(args) % 2 == 0:
 		kwargs = {}
@@ -26,15 +30,27 @@ class Management(commands.Cog):
 	@commands.command()
 	@commands.has_role(MODERATION_ROLES)
 	async def edit(self, ctx, *args):
+		emote = discord.utils.get(ctx.guild.emojis, id=EMOJI_ID)
 		if (category := ctx.channel.category):
 			if category.id == Categories.in_progress:
 				if (options := convert_args(args)):
-					for value in options.values():
-						if not value.isdigit():
-							await ctx.send(embed=discord.Embed(description=f'{emote} Please use only digits.', colour=COLOUR))
-							break
+					is_true = all([True if value.isdigit() else False for value in options.values()])
+					if is_true:
+						# Getting the message and editing it with the new embed
+						pins = await ctx.channel.pins()
+						message = pins[0]
+						embed = message.embeds[0]
+
+						new_embed = edit_embed(embed, options)
+
+						await message.edit(embed=new_embed)
+						# Editing the database
+						id = return_int(new_embed.title)
+						# await database.edit(id, data=options)
+					else:
+						await ctx.send(embed=discord.Embed(description=f'{emote} Please use only digits.', colour=COLOUR))
 				else:
-					await ctx.send(embed=discord.Embed(description=f'{emote} Please use valid arguments **(priority, amount, storage, discount)**', colur=COLOUR))
+					await ctx.send(embed=discord.Embed(description=f'{emote} Please use valid arguments **(priority, amount, storage, discount)**', colour=COLOUR))
 			else:
 				await ctx.send(embed=discord.Embed(description=f'{emote} Please use this command in a valid channel.', colour=COLOUR))
 		else:
--- bot/resources/functions.py
+++ bot/resources/functions.py
@@ -3,7 +3,7 @@ import discord
 import datetime
 from .constants import proceed_message
 
-__all__ = ['date', 'return_int', 'edit_embed', '_is_order_embed', '_is_proceed_embed']
+__all__ = ['date', 'return_int', 'edit_embed', 'edit_progress', '_is_order_embed', '_is_proceed_embed', 'total_price']
 
 def date():
     return datetime.datetime.now().strftime('%d.%m.%Y')
@@ -13,7 +13,7 @@ def return_int(s: str):
     return int(''.join([l for l in s if l.isdigit()]))
 
 
-def edit_embed(embed, **options):
+def edit_embed(embed, options):
     dct = embed.to_dict()
     fields = dct['fields']
     for field in fields:
@@ -22,6 +22,15 @@ def edit_embed(embed, **options):
             field['value'] = options[name.lower()]
 
     return discord.Embed.from_dict(dct)
+
+
+def edit_progress(embed, progress):
+    dct = embed.to_dict()
+    fields = dct['fields']
+    for field in fields:
+        name, value, _ = field.values()
+        if name.lower() == 'progress':
+            field['value'] = str(progress)
 
 
 title_pattern = '\*GS-\d+\*'
@@ -62,3 +71,18 @@ def _is_proceed_embed(message):
 
     except Exception:
         return False
+
+
+def total_price(amount: int, price_each: int, storage_fee: int, discount: int=0, priority: bool=False):
+    item_price = amount * price_each
+    total_storage_fee = amount * storage_fee
+
+    total_price = item_price + total_storage_fee
+
+    if discount:
+        total_price = round(total_price * (1 - (discount / 100)))
+
+    if priority:
+        total_price = round(total_price * 1.25)
+
+    return total_price
